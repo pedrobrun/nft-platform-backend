397c7c663847da92cae812f5613ab0f9718e0ce0
pedrobrun <pedroluccavb@gmail.com>
2022-02-02 19:25:37 -0300

    feat: add response messages enum

---
+++ src/domains/user/Enums.ts
@@ -0,0 +1,13 @@
+export enum ErrorMessages {
+  PASSWORD_CONFIRMATION = "Password confirmation doesn't match.",
+  NO_DATA = "Required data was not given to authenticate.",
+  AUTH_FAIL = "We were not able to authenticate your login :(",
+  ACCESS_DENIED = "Access denied.",
+  INVALID_TOKEN = "Invalid token.",
+  REGISTER_FAIL = "We were not able to register your user :("
+}
+
+export enum SuccessMessages {
+  AUTH_SUCC = "User successfully authenticated! :)",
+  REGISTER_SUCC = "Registration successfull :)"
+}
