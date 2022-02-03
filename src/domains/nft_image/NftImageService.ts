06bb4fd9b49b8f716637c4502b79261c001406e1
pedrobrun <pedroluccavb@gmail.com>
2022-02-03 02:15:36 -0300

    feat: add nft image service layer

---
+++ src/domains/nft_image/NftImageService.ts
@@ -0,0 +1,24 @@
+import { response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { NftImageInterface, NftImageModel } from "./NftImage";
+
+export class NftImageService {
+  // TODO: implement repository layer for decoupling
+  constructor() {}
+
+  public async createNftImage(nft: NftImageInterface) {
+
+    const { key, size, url } = nft;
+
+    const model = new NftImageModel({
+      key,
+      size,
+      url,
+    });
+
+    return model.save();
+    
+  }
+
+}
