f21f93cd8bc746f1dbd9a612be011fa09d4e25ae
pedrobrun <pedroluccavb@gmail.com>
2022-02-03 02:14:52 -0300

    feat: add nft model

---
+++ src/domains/nft_image/NftImage.ts
@@ -0,0 +1,25 @@
+import {model, Schema} from 'mongoose';
+import aws from 'aws-sdk';
+
+const s3 = new aws.S3();
+
+export interface NftImageInterface {
+  size: number,
+  key: string,
+  url: string,
+}
+
+const NftImageSchema = new Schema<NftImageInterface>({
+  size: { type: Number },
+  key: { type: String },
+  url: { type: String},
+}, {timestamps: true});
+
+NftImageSchema.pre('remove', function() {
+  s3.deleteObject({
+    Bucket: process.env.BUCKET_NAME,
+    Key: this.key,
+  }).promise();
+})
+
+export const NftImageModel = model('NftImage', NftImageSchema)
