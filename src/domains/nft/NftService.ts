0ce7b1b648d5b8859377b187de710da7e2e2218d
pedrobrun <pedroluccavb@gmail.com>
2022-02-03 16:50:48 -0300

    feat: fix var names in nft service layer

--- src/domains/nft/NftService.ts
+++ src/domains/nft/NftService.ts
@@ -5,70 +5,45 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { NftInterface, NftModel } from "./Nft";
 import { userRouter } from "../user/UserController";
+import { NftImageService } from "../nft_image/NftImageService";
+import { NftImageInterface } from "../nft_image/NftImage";
 
+/**
+ * TODO: implement Repository layer for more decoupling and dependency injection
+ */
 export class NftService {
-  // TODO: implement repository layer for decoupling
-  constructor() {}
+  private nftImageService: NftImageService;
+  
+  constructor(nftImageService: NftImageService) {
+    this.nftImageService = nftImageService;
+  }
 
   public async createNft(nft: NftInterface) {
 
-    const { title, description, usdFloorPrice, creator } = nft;
+    const { title, description, usdFloorPrice, creator, file } = nft;
 
     const model = new NftModel({
+      title,
       creator,
       description,
-      title,
-      usdFloorPrice
+      usdFloorPrice,
+      file
     });
 
     return model.save();
     
   }
 
-  public async authUser(userData: UserInterface) {
-    const existingUser = await UserModel.findOne({
-      username: userData.username,
-    });
-    if (!existingUser) {
-      return null;
-    }
-
-    const checkPassword = await bcrypt.compare(
-      userData.password,
-      existingUser.password
-    );
-
-    if (!checkPassword) {
-      return null;
-    }
-
-    try {
-      const secret: string = `${process.env.SECRET}`;
-      const token = jwt.sign({ id: existingUser._id }, secret);
-      return token;
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
   public async getAll(){
-    const nfts = await NftModel.find();
-    return nfts;
+    return NftModel.find();
   }
 
   public async delete(id: string){
-    const nftToDelete = await NftModel.findById(id);
-
-    try{
-      return NftModel.deleteOne({nftToDelete});
-    } catch (err) {
-      console.log(err)
-    }
+    return NftModel.findOneAndDelete(id);
   }
 
   public async findById(id: string){
     return UserModel.findById(id, '-password')
   }
 
-  public async checkIfExists(user: UserInterface) {}
 }
