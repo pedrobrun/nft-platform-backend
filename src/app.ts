3797a7049443aadc52da95584c5cba257a1d0e0b
pedrobrun <pedroluccavb@gmail.com>
2022-02-05 13:03:00 -0300

    refactor: rearrange app entry

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const path = require('path');
 import {GridFsStorage} from 'multer-gridfs-storage';
 const crypto = require('crypto');
 import cors from 'cors';
-
+import http from 'http';
 // file imports
 import {indexRouter} from "./index";
 import {userRouter} from "./domains/user/UserController";
@@ -16,13 +16,21 @@ import {nftImageRouter} from "./domains/nft_image/NftImageController"
 const app: Express = express();
 dotenv.config();
 
+const dbUri = process.env.MONGO_URI;
+
+mongoose.connect(`${dbUri}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+},).then(() => {
+  app.listen(process.env.PORT);
+  console.log("Connected to database");
+}).catch((err) => console.log(err));
+
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(cors());
 
-
-// env
-const dbUri = process.env.MONGO_URI;
 const dbPort = process.env.DB_PORT;
 
 const storage = new GridFsStorage({
@@ -46,21 +54,12 @@ const storage = new GridFsStorage({
   }
 });
 
+
 // routes
 app.use("/", indexRouter);
 app.use("/user", userRouter);
-
 /**
  * be aware that user controller login method calls this url statically, like 'nft/list/'
  */
 app.use("/nft", nftRouter)
-
 app.use("/nft-image", nftImageRouter);
-
-mongoose.connect(`${dbUri}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-},).then(() => {
-  app.listen(3000);
-  console.log("Connected to database");
-}).catch((err) => console.log(err));
