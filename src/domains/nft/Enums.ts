a5450371811058233573c347853fb83a203f5b81
pedrobrun <pedroluccavb@gmail.com>
2022-02-05 13:03:13 -0300

    feat: nft enums

---
+++ src/domains/nft/Enums.ts
@@ -0,0 +1,10 @@
+export enum ErrorMessages {
+  NO_DATA = 'Required data was not given',
+  FAIL_DELETE = 'We were not able to delete',
+  ACC_DENIED = 'Access denied'
+}
+
+export enum SuccessMessages {
+  SUCC_DELETE = 'Successfully deleted',
+  NO_NFTS_SHOW = 'There are no NFTs to show yet.'
+}
